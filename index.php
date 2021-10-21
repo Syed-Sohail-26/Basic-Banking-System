4150bb35ec01012df86f6a3e3f835b239c6c29c8
Syed-Sohail-26 <92902203+Syed-Sohail-26@users.noreply.github.com>
2021-10-21 08:41:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Edilla Bank </title>
+   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- Navigation bar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Edilla Bank</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="customers.php">Our Customers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="transactions.php">Transfer History</a>
+        </li>
+        <body style="background : pink">
+        
+        
+</div>
+
+
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+
+
+<!--About us-->
+
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >About Us</h3>
+ </div>
+
+<div class="container-fluid">
+ <div class ="row">
+<div  class="col-lg-6 col-md-6 col-12">
+      <img src="images/2.jpg" class="img-fluid">
+      </div>
+      <div  class="col-lg-6 col-md-6 col-12">
+      <h1>Edilla Bank</h1>
+      <p> Edilla Bank is a part of Edilla foundation. It is one of Znations's leading private banks and was among the first to receive approval from the Central bank of Znation (CBZ) to set up a private sector bank in 1992 ZY.
+          Today, Edilla Bank has a banking network of 5,608 branches and 16,087 ATM's in 2,902 cities/towns.
+          Edilla Bank offers a diverse range of financial products and banking services to customers(Which are dead actually, because they are EdillaS) through a growing branch and ATM network and digital channels such as Netbanking, Phonebanking and MobileBanking.
+      </p> 
+        <p>
+          <h4>Easy Money Transfer</h4>
+        </p> 
+        
+        <p>
+          <h4>Less Transaction Charges</h4>
+        </p>
+
+        <p>
+          <h4>Highly Secure Payment Gateway</h4>
+        </p>
+        </div>  
+         
+
+ </div>
+ </div>
+
+<!--Dsiclaimer-->
+
+</section>
+<section class= "my-5">
+<div class="py-5">
+ <h3 class="text-center" >Disclaimer</h3>
+ <p1>This website is the
+      outcome of a intern project, and does not
+      necessarily represent the views of any organisation or any other individuals referenced or
+      acknowledged within the website. Anyone may reproduce, distribute, adapt, translate the content on the website, without explicit permission, provided that the content is accompanied by an acknowledgement that Edilla Bank website is the source and that it is clearly indicated if changes were made to the original content.</p1>
+ </div>
+
+
+
+<footer class="text-center mt-2 py-5">
+        <p>&copy 2021  <b>Syed Sohail</b> </br>Chairman, Edilla Foundation</p>
+      </footer>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>  
+</body>
+</html>
